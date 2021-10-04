5776a2b730e055c5462dede78d744d9408fdae2e
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-04 16:12:31 +0100

    Bulk commit upto Sinatra Views

--- app.rb
+++ app.rb
@@ -2,13 +2,24 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get "/" do
+  @name = ["John", "Bob", "Rachel"].sample
   erb(:index)
+end
+
+get "/named-cat" do
+  p params
+  @name = params["name"]
+  erb(:named_cat)
 end
 
 get "/secret" do
   "Shhh!"
 end
 
-get "/cat" do
+get "/random-cat" do
   erb(:cat)
+end
+
+get "/u/:name" do
+  "<h1>Hello #{params["name"]}"
 end
